28e7ec341e3293d502d95a8c5fdd71d037caf489
kyle sadler <kylesadler5@gmail.com>
2021-04-14 18:01:13 -0500

    update setup

--- db_scripts/clear.bash
+++ db_scripts/clear.bash
@@ -1,16 +1,9 @@
-mysql
-
-use krs028
-
 SET FOREIGN_KEY_CHECKS = 0;
-drop tables ITEM;
-drop tables SUPPLIER;
-drop tables SALES;
-drop tables INVENTORY_MGMT;
-drop tables EMPLOYEE;
+drop tables PLAYER;
+drop tables TEAM;
+drop tables GAME;
+drop tables RESULT;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
 show tables;
-
-exit
