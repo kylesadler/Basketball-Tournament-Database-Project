64790abcbde73f67be6ab65c7ddbbcb9412741e2
kyle sadler <kylesadler5@gmail.com>
2021-04-14 12:11:54 -0500

    wacky updates

--- add_result.php
+++ add_result.php
@@ -1,6 +1,9 @@
 <html>
 <body>
-<?php require 'nav.php'; ?>
+<?php
+    require 'nav.php';
+    require "util.php";
+?>
 <h3>Add a new team:</h3>
 
 <form action="add_result.php" method="post">
@@ -17,29 +20,9 @@
 <?php
 if (isset($_POST['submit'])) 
 {
-    // add ' ' around multiple strings so they are treated as single command line args
-    $valid_params = array("game", "home", "away");
+    $params = parse_args($_POST, array("game", "home", "away"))
 
-    include "upload_valid_params.php";
-
-    $params = array();
-
-    foreach ($_POST as $param_name => $param_val) {
-        if (in_array($param_name, $valid_params) {
-            array_push($params, escapeshellarg($_POST[$param_name]));
-        }
-        echo "Param: $param_name; Value: $param_val<br />\n";
-    }
-
-    // build the linux command that you want executed;  
-    $command = 'python3 backend/add_result.py ' . implode(' ', $params);
-
-    // remove dangerous characters from command to protect web server
-    $command = escapeshellcmd($command);
- 
-    // echo then run the command
-    echo "command: $command <br>";
-    system($command);           
+    run_command('add_result', $params);
 }
 ?>
 
---
+++ util.php
@@ -0,0 +1,25 @@
+<?php
+
+function run_command($python_command, $params) {
+    $command = 'python3 backend/' . $python_command . '.py ' . implode(' ', $params);
+    
+    // remove dangerous characters from command to protect web server
+    $command = escapeshellcmd($command);
+    
+    echo "command: $command <br>";
+    system($command);    
+}
+
+function parse_args($post_body, $valid_params) {
+    $params = array();
+    
+    foreach ($post_body as $param_name => $param_val) {
+        if (in_array($param_name, $valid_params) {
+            array_push($params, escapeshellarg($post_body[$param_name]));
+        }
+        echo "Param: $param_name; Value: $param_val<br />\n";
+    }
+
+    return $params;
+}
+?>
