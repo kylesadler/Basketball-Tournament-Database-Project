e13317544e1364dafdbb1c38312e4ecccc93594f
kyle sadler <kylesadler5@gmail.com>
2021-04-14 12:41:56 -0500

    fix bug

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,6 @@
 import sys
 
 function = sys.argv[1]
-args = sys.argv[1:]   
+args = sys.argv[2:]   
 
-print(f"running {function} on arguments {', '.join(args)} <br> <br>")
+print(f"running {function} on arguments: {', '.join(args)} <br> <br>")
