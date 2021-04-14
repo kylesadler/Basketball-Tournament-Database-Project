aafd05ff8b41c308158abcacf09924be2310857c
kyle sadler <kylesadler5@gmail.com>
2021-04-14 18:18:37 -0500

    fix typo

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <li> <a href="/~krs028/add_game.php">Add a Game</a> </li>
     <li> <a href="/~krs028/add_result.php">Add a Result</a> </li>
     <li> <a href="/~krs028/view_teams.php">View Teams</a> </li>
-    <li> <a href="/~krs028/reults_by_team.php">View Results by Team</a> </li>
-    <li> <a href="/~krs028/reults_by_date.php">View Results by Date</a> </li>
+    <li> <a href="/~krs028/results_by_team.php">View Results by Team</a> </li>
+    <li> <a href="/~krs028/results_by_date.php">View Results by Date</a> </li>
     <li> <a href="/~krs028/team_rosters.php">View Team Rosters</a> </li>
 </ol>
 
