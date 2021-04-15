947c175ea4fdb05faf9d8bf8993757c12bd62719
kyle sadler <kylesadler5@gmail.com>
2021-04-14 22:05:24 -0500

    refactor

--- database/api.py
+++ database/api.py
@@ -1,75 +1,11 @@
 import sys
-from sql_database import SQLDatabase
-# from util import *
 import util
+from sql_database import SQLDatabase
 
-""" 
-
-arguments cleaned and passed to functions automatically
-NOTE: return a list of objects. May be html strings
-
-"""
 
 MYSQL_USER = 'krs028'
 MYSQL_PASS ='PhoV9bi2'
 database = SQLDatabase('localhost', MYSQL_USER, MYSQL_PASS, MYSQL_USER)
-
-
-""" 
-
-        query = f'''SELECT 
-                        SUPPLIER.ID AS SUPPLIER_ID,
-                        SUPPLIER.NAME AS SUPPLIER_NAME,
-                        SUPPLIER.PHONE_NUMBER AS SUPPLIER_PHONE_NUMBER,
-                        ITEM.NAME AS COFFEE_NAME,
-                        ITEM.ROASTING_TYPE
-                    FROM INVENTORY_MGMT
-                    INNER JOIN ITEM
-                    INNER JOIN SUPPLIER
-                    WHERE
-                        SUPPLIER.COUNTRY = '{clean_input(country)}'
-                        AND INVENTORY_MGMT.ITEM_ID = ITEM.ID
-                        AND INVENTORY_MGMT.SUPPLIER_ID = SUPPLIER.ID;'''
-        
-
-        headers, rows = self.database.select(query)
-
-
-
-
-
-
-        supplier_id = self.generate_unique_id('SUPPLIER')
-        self.database.insert('SUPPLIER', f"{supplier_id},'{name}',{number},'{country}'")
-
-        self.database.insert('INVENTORY_MGMT', f"{item_id},{supplier_id},0,0")
-
-
-        # print new supplier and other suppliers with same item
-        print("New supplier created.")
-        print_table(*self.database.select(f"SELECT * FROM SUPPLIER WHERE ID = {supplier_id};"))
-
-        print("\nOther suppliers that provide the same item:")
-        print_table(*self.database.select(
-            f'''SELECT
-                SUPPLIER.NAME
-            FROM INVENTORY_MGMT
-            INNER JOIN SUPPLIER
-            WHERE
-                SUPPLIER.ID = INVENTORY_MGMT.SUPPLIER_ID
-                AND INVENTORY_MGMT.ITEM_ID = {item_id}
-                AND INVENTORY_MGMT.SUPPLIER_ID != {supplier_id};'''
-        ))
-
-
-
-        self.database.update(
-            f'''Update INVENTORY_MGMT
-            SET TOTAL_AVAILABLE = {new_available}
-            WHERE ITEM_ID = {item_id};'''
-            )
-
-"""
 
 
 def add_game(home, away, court, date):
@@ -77,15 +13,12 @@ def add_game(home, away, court, date):
     database.insert('GAME', f"{_id},{court},{home},{away},STR_TO_DATE('{date}','%m/%d/%Y')")
 
 def add_team(name, mascot, seed):
-
     _id = database.generate_unique_id('TEAM')
     database.insert('TEAM', f"{_id},'{name}','{mascot}',{seed}")
-
 
 def add_result(game, home, away):
     _id = database.generate_unique_id('RESULT')
     database.insert('RESULT', f"{_id},{game},{home},{away}")
-
 
 def add_player(name, position, team_id):
     _id = database.generate_unique_id('PLAYER')
@@ -95,12 +28,28 @@ def add_player(name, position, team_id):
 def get_teams():
     return database.select('SELECT * FROM TEAM;')
 
-
 def get_results():
     return database.select('SELECT * FROM RESULT;')
 
 def get_games():
     return database.select('SELECT * FROM GAME;')
+
+def get_players():
+    return database.select(
+        '''SELECT 
+            PLAYER.NAME AS NAME,
+            PLAYER.POSITION AS POSITION,
+            CONCAT(TEAM.NAME, ' ', TEAM.MASCOT) AS TEAM
+        FROM PLAYER
+        INNER JOIN TEAM
+            WHERE PLAYER.TEAM_ID = TEAM.ID;'''
+        )
+
+def get_roster():
+    return database.select('SELECT * FROM PLAYER;')
+
+def get_team_name_mascot_id():
+    return database.select('SELECT NAME, MASCOT, ID FROM TEAM;')
 
 def get_games_and_results():
     return database.select(
@@ -120,17 +69,6 @@ def get_games_and_results():
             ON GAME.ID = RESULT.GAME_ID;'''
         )
 
-def get_players():
-    return database.select(
-        '''SELECT 
-            PLAYER.NAME AS NAME,
-            PLAYER.POSITION AS POSITION,
-            CONCAT(TEAM.NAME, ' ', TEAM.MASCOT) AS TEAM
-        FROM PLAYER
-        INNER JOIN TEAM
-            WHERE PLAYER.TEAM_ID = TEAM.ID;'''
-        )
-
 def get_result_dates():
     return database.select(
         '''SELECT 
@@ -145,17 +83,6 @@ def get_court_numbers():
     return database.select(
         'SELECT COURT_NUM FROM GAME GROUP BY COURT_NUM;'
         )
-
-def get_roster():
-    return database.select('SELECT * FROM PLAYER;')
-
-
-def get_team_name_mascot_id():
-    query = f'''SELECT 
-        NAME, MASCOT, ID
-    FROM TEAM;'''
-
-    return database.select(query)
 
 
 def get_results_by_team_id(_id):
@@ -181,7 +108,6 @@ def get_results_by_team_id(_id):
 
     return database.select(query)
 
-
 def get_results_by_date(date):
     query = f'''SELECT
                 GAME.HOME_TEAM_ID AS HOME_ID,
@@ -202,7 +128,6 @@ def get_results_by_date(date):
 
     return database.select(query)
 
-
 def get_games_by_court(court_num):
     query = f'''SELECT
                GAME.HOME_TEAM_ID AS HOME_ID,
@@ -222,7 +147,6 @@ def get_games_by_court(court_num):
 
     return database.select(query)
 
-
 def get_roster_by_team(_id):
     return database.select(
         f'''SELECT 
@@ -238,25 +162,27 @@ def get_roster_by_team(_id):
 
 
 COMMAND_TO_FUNCTION = {
+    # add functions
     'add_game': add_game,
     'add_team': add_team,
     'add_result': add_result,
     'add_player': add_player,
 
+    # static functions
     'get_teams': get_teams,
     'get_players': get_players,
     'get_roster': get_roster,
     'get_games': get_games,
     'get_results': get_results,
-
     'get_games_and_results': get_games_and_results,
     'get_team_name_mascot_id': get_team_name_mascot_id,
-    'get_results_by_team_id': get_results_by_team_id,
-    'get_results_by_date': get_results_by_date,
-    'get_roster_by_team': get_roster_by_team,
     'get_result_dates': get_result_dates,
     'get_court_numbers': get_court_numbers,
 
+    # parametarized functions
+    'get_results_by_team_id': get_results_by_team_id,
+    'get_results_by_date': get_results_by_date,
+    'get_roster_by_team': get_roster_by_team,
     'get_games_by_court': get_games_by_court,
 }
 
