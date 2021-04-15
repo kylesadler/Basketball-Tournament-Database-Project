caf8ebd5c94708256c44965214829025bfa6290b
kyle sadler <kylesadler5@gmail.com>
2021-04-14 20:07:11 -0500

    merge games and results

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
 
 <ol>
     <li> <a href="/~krs028/manage_teams.php">Manage Teams</a> </li>
-    <li> <a href="/~krs028/manage_games.php">Manage Games</a> </li>
-    <li> <a href="/~krs028/manage_results.php">Manage Results</a> </li>
+    <li> <a href="/~krs028/manage_games.php">Manage Games and Scores</a> </li>
     <li> <a href="/~krs028/manage_rosters.php">Manage Rosters</a> </li>
     <li> <a href="/~krs028/results_by_team.php">View Results by Team</a> </li>
     <li> <a href="/~krs028/results_by_date.php">View Results by Date</a> </li>
