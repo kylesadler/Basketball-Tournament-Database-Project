e7059128b8f668e8a020f9fb78f72a6fd22f3548
kyle sadler <kylesadler5@gmail.com>
2021-04-14 20:04:06 -0500

    fix

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 ?>
 
 <ol>
-    <li> <a href="/~krs028/manage_team.php">Manage Teams</a> </li>
-    <li> <a href="/~krs028/manage_game.php">Manage Games</a> </li>
-    <li> <a href="/~krs028/manage_result.php">Manage Results</a> </li>
+    <li> <a href="/~krs028/manage_teams.php">Manage Teams</a> </li>
+    <li> <a href="/~krs028/manage_games.php">Manage Games</a> </li>
+    <li> <a href="/~krs028/manage_results.php">Manage Results</a> </li>
     <li> <a href="/~krs028/manage_rosters.php">Manage Rosters</a> </li>
     <li> <a href="/~krs028/results_by_team.php">View Results by Team</a> </li>
     <li> <a href="/~krs028/results_by_date.php">View Results by Date</a> </li>
---
+++ manage_rosters.php
@@ -0,0 +1,33 @@
+<html>
+<body>
+<?php
+    require 'util/style.php';
+    require 'util/nav.php';
+    require 'util/util.php';
+?>
+<h3>Add a new player:</h3>
+
+<form action="add_team.php" method="post">
+    Name: <input type="text" name="name"><br>
+    Mascot: <input type="text" name="mascot"><br>
+    Tournament Seed: <input type="text" name="tournamentSeed"><br>
+    <input name="submit" type="submit" >
+</form>
+
+
+<?php
+if (isset($_POST['submit'])) 
+{
+    $args = parse_args($_POST, array("name", "mascot", "tournamentSeed"));
+
+    database('add_team', $args);
+}
+
+list($headers, $rows) = database('get_teams');
+print_table($headers, $rows);
+?>
+
+<br><br>
+
+</body>
+</html>
