d9ec7003d687e3b780f2e0abeed4c2a8013d9983
kyle sadler <kylesadler5@gmail.com>
2021-04-14 21:29:00 -0500

    update by date

--- results_by_date.php
+++ results_by_date.php
@@ -17,8 +17,29 @@ if (isset($_POST['submit']))
 {
     $args = parse_args($_POST, array("date"));
 
-    list($headers, $rows) = database('get_results_by_date', $args);
+    list($keys, $values) = database('get_results_by_date', $args);
     
+    
+    $headers = array("Home", "Away", "Score", "Court", "Date");
+    $rows = array();
+    
+    foreach ($values as $i => $value) {
+        
+        $data = array_combine($keys, $value);
+
+        $result = ($data['HOME_ID'] == $_POST['teamID']) == ($data['HOME_SCORE'] > $data['AWAY_SCORE']);
+
+        $row = array(
+            $data['HOME_NAME'].' '.$data['HOME_MASCOT'],
+            $data['AWAY_NAME'].' '.$data['AWAY_MASCOT'],
+            $data['HOME_SCORE'].'-'.$data['AWAY_SCORE'],
+            $data['COURT_NUMBER'],
+            $data['DATE']
+        );
+
+        array_push($rows, $row);
+    }
+
     print_table($headers, $rows);
     
     echo '<script> document.getElementById("date").value = '.$args[0].'; </script>';
